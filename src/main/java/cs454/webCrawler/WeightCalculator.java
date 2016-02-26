579cbbad6cd8064bcec2bc16983c3bcc5ecce217
ElTangoBandito <Volfurious@gmail.com>
2016-02-26 10:56:39 -0800

    Quick alteration to demo.

--- src/main/java/cs454/webCrawler/WeightCalculator.java
+++ src/main/java/cs454/webCrawler/WeightCalculator.java
@@ -1,7 +1,11 @@
 package cs454.webCrawler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.json.simple.JSONObject;
 
@@ -10,10 +14,12 @@ public class WeightCalculator {
 	private int numberOfDocuments = 0;
 	private JSONObject documents;
 	private List<String> listOfWords = new ArrayList<String>();//change this to set, so that it doesn't have repeating words
+	private Set<String> query = new HashSet<String>();
 	
-	public WeightCalculator(JSONObject JObj, int numberOfDocuments){
+	public WeightCalculator(String queryIn, JSONObject JObj, int numberOfDocuments){
 		this.numberOfDocuments = numberOfDocuments;
 		this.documents = JObj;
+		query.addAll(Arrays.asList(queryIn.toLowerCase().split(" ")));
 	}
 	
 	//find all the words from all the documents
@@ -28,5 +34,9 @@ public class WeightCalculator {
 		double tf = Math.log(1 + frequency);
 		double weight = tf * idf;
 		return weight;
+	}
+	
+	public Set<String> getQuery(){
+		return query;
 	}
 }
--- src/main/java/cs454/webCrawler/demo.java
+++ src/main/java/cs454/webCrawler/demo.java
@@ -1,6 +1,7 @@
 package cs454.webCrawler;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,21 +21,40 @@ import com.google.gson.GsonBuilder;
 public class demo {
 	static JSONArray jsonArray = new JSONArray();
 	
+	@SuppressWarnings("unchecked")
 	public static void main( String[] args ) throws IOException, SAXException, TikaException, ParseException, JSONException
     {
-		File file = new File("C:/Users/Volfurious/Desktop/test.html");//file path 
-		WordCounter word = new WordCounter(file);
+		File file = new File("C:/Users/Volfurious/Desktop/testfiles");//file path 
+		File[] htmlFiles = file.listFiles(new FilenameFilter(){
+			public boolean accept(File dir, String name){
+				return name.endsWith("html");
+			}
+		});
 		
+		JSONObject finalObj = new JSONObject();
+		for (File f: htmlFiles){
+			WordCounter word = new WordCounter(f);
+			finalObj.put(f.getName(), word.getJson());
+		}
+		
+		WeightCalculator wc = new WeightCalculator("dSg cAt mice mice", finalObj, 20);
+		//System.out.println(wc.getQuery().toString());
+		//WordCounter word = new WordCounter(file);
+		
+		/*
 		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
 		String pretJson = prettyGson.toJson(word.getJson());
 		
 		JSONParser parser = new JSONParser();
 		Object obj = parser.parse(pretJson);
 		JSONObject testObj = (JSONObject) obj;
+		*/
 
 		for (@SuppressWarnings("rawtypes")
-		Iterator iterator = testObj.keySet().iterator(); iterator.hasNext();){
+		Iterator iterator = finalObj.keySet().iterator(); iterator.hasNext();){
 			String key = (String) iterator.next();
+			System.out.println(key.toString());
+			/*
 			JSONObject innerObj = (JSONObject) testObj.get(key);
 			
 			JSONArray pos = (JSONArray) innerObj.get("position");
@@ -45,6 +65,7 @@ public class demo {
 				tempList.add(Integer.parseInt(numIterator.next()));
 			}
 			System.out.println(innerObj.get("word") + ": " + innerObj.get("frequency") + " " + tempList.toString());
+			*/
 		}
     }
 }
